c46b529e980082b312ca8e99e8f78cc8e9a3a28c
GameDev4Fun <valere@gamedev4fun.io>
2023-04-14 18:48:00 -0400

    Initial commit

---
+++ client/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <my_http_parser/client.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    //Create a client allowing to do request on certain URL (see the PDF at the root)
+    return 0;
+}
---
+++ server/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+#include <my_http_parser/server.h>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    //Create a server serving HTTP content (see the PDF for more infos)
+    return 0;
+}
